cf4945f4ce8cd9c63aaec55d4310accc2c94c032
JHONDRASCODE <sebastian_ramirezro@soy.sena.edu.co>
2024-06-07 22:03:33 -0500

    se hizo algo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Ejemplo de cómo podrías obtener datos dinámicos con JavaScript
+    document.getElementById('client-count').innerText = 100; // Ejemplo de datos
+    document.getElementById('order-count').innerText = 50;  // Ejemplo de datos
+    document.getElementById('product-count').innerText = 200; // Ejemplo de datos
+});
+
+
